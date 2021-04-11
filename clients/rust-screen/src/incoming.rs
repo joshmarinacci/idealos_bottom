8fa853c03aab288aef770e1e80599d83fce74066
josh <joshua@marinacci.org>
2021-04-11 13:07:43 -0700

    refactor to more files

---
+++ clients/rust-screen/src/incoming.rs
@@ -0,0 +1,86 @@
+use core::option::Option::None;
+use core::result::Result::{Err, Ok};
+use serde_json::error::Result;
+use serde_json::value::Value;
+use websocket::receiver::Reader;
+use std::net::TcpStream;
+use std::sync::mpsc::Sender;
+use websocket::OwnedMessage;
+use crate::messages::{RenderMessage, WindowListMessage, OpenWindowScreen, DrawPixelMessage, FillRectMessage, DrawImageMessage, CloseWindowScreen};
+
+fn parse_message(sender:&Sender<OwnedMessage>,  renderloop_send:&Sender<RenderMessage>, txt:String) -> Result<()>{
+    let v: Value = serde_json::from_str(txt.as_str())?;
+    match &v["type"] {
+        Value::String(msg_type) => {
+            match &msg_type[..] {
+                "SCREEN_WINDOW_LIST" => {
+                    let msg:WindowListMessage = serde_json::from_str(txt.as_str())?;
+                    renderloop_send.send(RenderMessage::WindowList(msg));
+                    ()
+                },
+                "WINDOW_OPEN_SCREEN" => {
+                    let msg:OpenWindowScreen = serde_json::from_str(txt.as_str())?;
+                    renderloop_send.send(RenderMessage::OpenWindow(msg));
+                    ()
+                },
+                "DRAW_PIXEL"  => {
+                    let msg:DrawPixelMessage = serde_json::from_str(txt.as_str())?;
+                    renderloop_send.send(RenderMessage::DrawPixel(msg));
+                    ()
+                },
+                "DRAW_RECT"   => {
+                    let msg:FillRectMessage = serde_json::from_str(txt.as_str())?;
+                    renderloop_send.send(RenderMessage::FillRect(msg));
+                    ()
+                },
+                "DRAW_IMAGE"   => {
+                    let msg:DrawImageMessage = serde_json::from_str(txt.as_str())?;
+                    renderloop_send.send(RenderMessage::DrawImage(msg));
+                    ()
+                },
+                "WINDOW_CLOSED" => {
+                    let msg:CloseWindowScreen = serde_json::from_str(txt.as_str())?;
+                    renderloop_send.send(RenderMessage::CloseWindow(msg));
+                    ()
+                }
+                _ => {
+                    println!("some other message type {}",txt)
+                }
+            }
+        }
+        _ => {
+            println!("data that's not a message!!")
+        }
+    }
+   Ok(())
+}
+
+pub fn process_incoming(receiver: &mut Reader<TcpStream>, websocket_sending_tx: &Sender<OwnedMessage>, render_loop_send: &Sender<RenderMessage>) {
+    // Receive loop
+    for message in receiver.incoming_messages() {
+        let message = match message {
+            Ok(m) => m,
+            Err(e) => {
+                println!("Receive Loop: {:?}", e);
+                let _ = websocket_sending_tx.send(OwnedMessage::Close(None));
+                return;
+            }
+        };
+        match message {
+            OwnedMessage::Close(_) => {
+                println!("got a close message");
+                // Got a close message, so send a close message and return
+                let _ = websocket_sending_tx.send(OwnedMessage::Close(None));
+                return;
+            }
+            // Say what we received
+            OwnedMessage::Text(txt) => {
+                // println!("received message {:?}", txt);
+                parse_message(websocket_sending_tx, render_loop_send, txt);
+            }
+            _ => {
+                println!("Receive Loop: {:?}", message);
+            },
+        }
+    }
+}
--- clients/rust-screen/src/main.rs
+++ clients/rust-screen/src/main.rs
@@ -15,68 +15,15 @@ use websocket::sender::Writer;
 use messages::{CloseWindowScreen, DrawImageMessage, DrawPixelMessage, FillRectMessage, MouseDownMessage, MouseUpMessage, OpenWindowScreen, RefreshWindowMessage, RenderMessage, WindowListMessage};
 use window::{Point, Window};
 
+use crate::incoming::process_incoming;
+use crate::outgoing::process_outgoing;
+
 mod messages;
 mod window;
+mod incoming;
+mod outgoing;
 
 const scale: i32 = 10;
-
-
-fn parse_message(sender:&Sender<OwnedMessage>,  renderloop_send:&Sender<RenderMessage>, txt:String) -> Result<()>{
-    let v: Value = serde_json::from_str(txt.as_str())?;
-    match &v["type"] {
-        Value::String(strr) => {
-            match &strr[..] {
-                "SCREEN_WINDOW_LIST" => {
-                    let msg:WindowListMessage = serde_json::from_str(txt.as_str())?;
-                    renderloop_send.send(RenderMessage::WindowList(msg));
-                    ()
-                },
-                "WINDOW_OPEN_SCREEN" => {
-                    let msg:OpenWindowScreen = serde_json::from_str(txt.as_str())?;
-                    renderloop_send.send(RenderMessage::OpenWindow(msg));
-                    ()
-                },
-                "DRAW_PIXEL"  => {
-                    let msg:DrawPixelMessage = serde_json::from_str(txt.as_str())?;
-                    renderloop_send.send(RenderMessage::DrawPixel(msg));
-                    ()
-                },
-                "DRAW_RECT"   => {
-                    let msg:FillRectMessage = serde_json::from_str(txt.as_str())?;
-                    renderloop_send.send(RenderMessage::FillRect(msg));
-                    ()
-                },
-                "DRAW_IMAGE"   => {
-                    let msg:DrawImageMessage = serde_json::from_str(txt.as_str())?;
-                    renderloop_send.send(RenderMessage::DrawImage(msg));
-                    ()
-                },
-                "WINDOW_CLOSED" => {
-                    let msg:CloseWindowScreen = serde_json::from_str(txt.as_str())?;
-                    renderloop_send.send(RenderMessage::CloseWindow(msg));
-                    ()
-                }
-                _ => {
-                    println!("some other message type {}",txt)
-                }
-            }
-        }
-        _ => {
-            println!("data that's not a message!!")
-        }
-    }
-   Ok(())
-}
-
-fn screen_name(msg:&OpenWindowScreen) {
-    println!("go screen name {:?}",msg)
-}
-fn draw_pixel(msg:&DrawPixelMessage) {
-    println!("drawing pixel from {:?}",msg);
-}
-fn fill_rect(msg:&FillRectMessage) {
-    println!("fill rect {:?}",msg);
-}
 
 fn main() {
     let mut windows:HashMap<String,Window> = HashMap::new();
@@ -90,21 +37,7 @@ fn main() {
 
     let mut active_window:Option<String> = Option::None;
 
-    let border_color = "black";
-
-    println!("Hello, world!");
     let name  = "ws://127.0.0.1:8081";
-/*
- once open, send a screen start message
- wait for incoming messages.
- if incoming is open_window allocate a window
- if incoming is window list, refresh the windows by sending refresh_window to each one
- if incoming is draw pixel, draw it
- if incoming is fill rect, draw it
- */
-
-
-
     let mut client = ClientBuilder::new(name)
         .unwrap()
         .connect_insecure()
@@ -112,19 +45,20 @@ fn main() {
 
     println!("we are connected now!");
 
-    let (mut receiver, mut sender) = client.split().unwrap();
+    let (mut server_reader, mut server_sender) = client.split().unwrap();
     //create a channel
     let (tx, websocket_sending_rx) = channel();
     let websocket_sending_tx = tx.clone();
     let (render_loop_send, render_loop_receive) = channel::<RenderMessage>();
 
     //loop for receiving
-    let receive_loop = thread::spawn(move || {  process_incoming_server_messages(&mut receiver, &websocket_sending_tx, &render_loop_send);  });
-
+    let receive_loop = thread::spawn(move || {
+        process_incoming(&mut server_reader, &websocket_sending_tx, &render_loop_send);
+    });
 
     //loop for sending
     let send_loop = thread::spawn(move || {
-        process_outgoing_server_messages(&websocket_sending_rx,&mut sender);
+        process_outgoing(&websocket_sending_rx, &mut server_sender);
     });
 
 
@@ -136,8 +70,6 @@ fn main() {
             println!("error sending: {:?}", e);
         }
     }
-
-
 
     // open window
     let (mut rl, thread) = raylib::init()
@@ -224,68 +156,6 @@ fn process_mouse_input(rl: &mut RaylibHandle, windows:&HashMap<String,Window>, w
                 };
                 websocket_sender.send(OwnedMessage::Text(json!(msg).to_string()));
             }
-        }
-    }
-}
-
-fn process_outgoing_server_messages(websocket_sending_rx: &Receiver<OwnedMessage>, sender: &mut Writer<TcpStream>) {
-    loop {
-        // Send loop
-        let message = match websocket_sending_rx.recv() {
-            Ok(m) => m,
-            Err(e) => {
-                println!("Send Loop: {:?}", e);
-                return;
-            }
-        };
-        match message {
-            OwnedMessage::Close(_) => {
-                let _ = sender.send_message(&message);
-                // If it's a close message, just send it and then return.
-                return;
-            }
-            _ => (),
-        }
-        // Send the message
-        // println!("sending out {:?}",message);
-        match sender.send_message(&message) {
-            Ok(()) => (),
-            Err(e) => {
-                println!("Send Loop: {:?}", e);
-                let _ = sender.send_message(&Message::close());
-                return;
-            }
-        }
-    }
-
-}
-
-fn process_incoming_server_messages(receiver: &mut Reader<TcpStream>, websocket_sending_tx: &Sender<OwnedMessage>, render_loop_send: &Sender<RenderMessage>) {
-    // Receive loop
-    for message in receiver.incoming_messages() {
-        let message = match message {
-            Ok(m) => m,
-            Err(e) => {
-                println!("Receive Loop: {:?}", e);
-                let _ = websocket_sending_tx.send(OwnedMessage::Close(None));
-                return;
-            }
-        };
-        match message {
-            OwnedMessage::Close(_) => {
-                println!("got a close message");
-                // Got a close message, so send a close message and return
-                let _ = websocket_sending_tx.send(OwnedMessage::Close(None));
-                return;
-            }
-            // Say what we received
-            OwnedMessage::Text(txt) => {
-                // println!("received message {:?}", txt);
-                parse_message(websocket_sending_tx, render_loop_send, txt);
-            }
-            _ => {
-                println!("Receive Loop: {:?}", message);
-            },
         }
     }
 }
