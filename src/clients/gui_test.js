289d206118c6fac526dd043a8c0ed93db85a4a72
josh <joshua@marinacci.org>
2021-04-17 14:57:40 -0700

    turn code back on

--- src/clients/gui_test.js
+++ src/clients/gui_test.js
@@ -14,28 +14,28 @@ async function init() {
             height:app.win.height,
             children:[
                 new Label({text:"label 1"}),
-                // new Label({text:"label 2"}),
-                // new Label({text:"label 3"}),
+                new Label({text:"label 2"}),
+                new Label({text:"label 3"}),
                 new Button({text:'button',width:30, height:15}),
                 new HBox({children:[
                     new Button({text:'button',width:30, height:15, id:'button'}),
                     new Label({text:'label',id:'button-target'}),
                 ]}),
-                // new TextBox({text:"hi",width:50, height: 15, id:'textbox'}),
-                // new HBox({children:[
-                //     new ToggleButton({text:'A', width:15, height:15}),
-                //     new ToggleButton({text:'B', width:15, height:15}),
-                // ]})
+                new TextBox({text:"hi",width:50, height: 15, id:'textbox'}),
+                new HBox({children:[
+                    new ToggleButton({text:'A', width:15, height:15}),
+                    new ToggleButton({text:'B', width:15, height:15}),
+                ]})
             ]})
         //attach actions
-        // app.win.root.find({id:'button'}).on('action',()=>{
-        //     app.win.root.find({id:'button-target'}).text = 'clicked!'
-        //     app.win.redraw()
-        // })
-        // app.win.root.find({id:'textbox'}).on('action',()=>{
-        //     app.win.root.find({id:'button-target'}).text = 'committed'
-        //     app.win.redraw()
-        // })
+        app.win.root.find({id:'button'}).on('action',()=>{
+            app.win.root.find({id:'button-target'}).text = 'clicked!'
+            app.win.redraw()
+        })
+        app.win.root.find({id:'textbox'}).on('action',()=>{
+            app.win.root.find({id:'button-target'}).text = 'committed'
+            app.win.redraw()
+        })
         //redraw
         app.win.redraw()
         //get the latest version of the theme
