9901764f11e5b93d89630f8b6dad657b8265ec52
josh <joshua@marinacci.org>
2021-04-22 08:41:59 -0700

    add first attempt at a schema compiler

--- src/clients/menubar.js
+++ src/clients/menubar.js
@@ -3,6 +3,107 @@ import {CommonApp, PixelFont} from './app_utils.js'
 import {Container} from './guitoolkit.js'
 let app = new CommonApp(process.argv,1024/4,10,'menubar')
 
+let menu_tree = {
+    type:'root',
+    children:[
+        {
+            type:'top',
+            label:'File',
+            children:[
+                {
+                    type:'item',
+                    label:'Open',
+                    event:'open_file',
+                    keystroke:{
+                        modifier:'meta',
+                        key:'O',
+                    }
+                },
+                {
+                    type:'item',
+                    label:'Close',
+                    event:'close_window',
+                    keystroke: {
+                        modifier: 'meta',
+                        key:'W',
+                    }
+                }
+            ]
+        },
+        {
+            type:'top',
+            label:'Edit',
+            children:[
+                {
+                    type:'item',
+                    label:'Cut',
+                    event:'clipboard_cut',
+                    keystroke: {
+                        modifier: 'meta',
+                        key:'X'
+                    }
+                },
+                {
+                    type:'item',
+                    label:'Copy',
+                    event:'clipboard_copy',
+                    keystroke: {
+                        modifier: 'meta',
+                        key:'C'
+                    }
+                },
+                {
+                    type:'item',
+                    label:'Paste',
+                    event:'clipboard_paste',
+                    keystroke: {
+                        modifier: 'meta',
+                        key:'V'
+                    }
+                },
+            ]
+        }
+    ]
+}
+
+
+/*
+syntax def for menu tree
+
+// base types
+string
+i32
+// compound types
+enum
+array
+object
+
+
+node_type = enum string "item" "top" "root"
+keystroke_obj = object modifier:string key:string
+item = object type:node_type label:string event:string keystroke:keystroke_obj
+
+generates schema JSON like this:
+class ITEM_STRUCT {
+   constructor(args) {
+        if(!args.hasOwnProperty('type')) throw new Error(`'Item' missing property 'type'`)
+        this.type = MAKE_NODE_TYPE_ENUM(args.type)
+        if(!args.hasOwnProperty('label')) throw new Error(`'Item' missing property 'label')
+        this.label = args.label
+        if(!args.hasOwnProperty('keystroke')) throw new Error(`'Item' missing property 'keystroke')
+        this.keystroke = MAKE_KEYSTROKE_STRUCT(args.keystroke)
+   }
+}
+function MAKE_NODE_TYPE_ENUM(value) {
+    if(value === "val1") return value
+    if(value === "val2") return value
+    throw new Error("enum node type: invalid value ${value" for enum "node_type))
+}
+let item = new Item_struct({type:'item', label:'Paste', event:'clipboard_paste', keystroke:{modifier:'meta',key:'V'})
+
+item.to_json() => turns into JSON string for sending over the wire
+ */
+
 
 class CustomMenuBar extends Container {
     constructor(opts) {
---
+++ tools/schema_compiler.js
@@ -0,0 +1,134 @@
+//parse schemas/menu.txt into src/schemas/menu.js
+
+
+import fs from 'fs'
+
+function log(...args) {
+    console.log(...args)
+}
+
+class SrcOutput {
+    constructor() {
+        this.lines = []
+        this.depth = 0
+        this.tab = "    "
+    }
+    line(str) {
+        let space = ""
+        for(let i=0; i<this.depth; i++) {
+            space += this.tab
+        }
+        this.lines.push(space+str)
+    }
+    toString() {
+        return this.lines.join("\n")
+    }
+
+    indent() {
+        this.depth++
+    }
+
+    outdent() {
+        this.depth--
+    }
+
+    blank() {
+        this.line("")
+    }
+}
+
+async function process_schema(src, dst_js, dst_rs) {
+    let schema = (await fs.promises.readFile(src)).toString()
+    console.log("schema",schema)
+    let lines = schema.split("\n")
+    let defs = {}
+    let gen_targets = []
+    lines.forEach(line => {
+        let [name, def_str] = line.split("=")
+        if(!name)return
+        name = name.trim()
+        if(name === '@GEN') {
+            gen_targets.push(def_str.trim())
+            return
+        }
+        if(name.startsWith("@")) return
+        let def_parts = def_str.trim().split(" ")
+        let def_type = def_parts.shift()
+        if(def_type === 'enum') {
+            defs[name] = {
+                type:'enum',
+                enum_type:def_parts.shift(),
+                values:def_parts.slice()
+            }
+        }
+        if(def_type === 'object') {
+            let props = {}
+            def_parts.forEach(pt => {
+                let [key,value] = pt.split(":")
+                props[key]= value
+            })
+            defs[name] = {
+                type:'object',
+                props:props
+            }
+        }
+    })
+    log("final defs",defs)
+    log("gen targets",gen_targets)
+
+    let js_src = ''
+    let output = new SrcOutput()
+    Object.keys(defs).forEach(target => {
+        let def = defs[target]
+        log("generating code for",target, def)
+        if(def.type === 'object') {
+            output.line(`function MAKE_${target}(data) {`)
+            output.indent()
+            output.line('let obj = {}')
+            Object.entries(def.props).forEach(([name,type])=>{
+                output.line(`if(!data.hasOwnProperty('${name}')) throw new Error("object '${target}' is missing property '${name}' ")`)
+                if(type === 'string') {
+                    output.line(`obj.${name} = data.${name}`)
+                    output.blank()
+                    return
+                }
+                output.line(`obj.${name} = MAKE_${type}(data.${name})`)
+                output.blank()
+            })
+            output.line(`return obj`)
+            output.outdent()
+            output.line('}')
+        }
+        if(def.type === 'enum') {
+            log("making an enum def",def)
+            output.line(`export function MAKE_${target}(value) {`)
+            output.indent()
+            def.values.forEach(val => {
+                output.line(`if(value === ${val}) return value`)
+            })
+            output.line(`throw new Error("MAKE_${target}: invalid value"+value)`)
+            output.outdent()
+            output.line("}")
+        }
+    })
+
+    output.line("export const MENUS = {")
+    output.indent()
+    Object.keys(defs).forEach(target => {
+        output.line(`MAKE_${target} : MAKE_${target},`)
+    })
+
+    output.outdent()
+    output.line("}")
+    // log("final source is\n",output.toString())
+    await fs.promises.writeFile(dst_js,output.toString())
+    log("wrote to ",dst_js)
+}
+
+async function doit() {
+    await process_schema('./tools/schemas/menus.txt',
+        './menus_schemas.js',
+        './menus_schemas.rs')
+}
+
+doit().then(()=>console.log("finished generating schemas"));
