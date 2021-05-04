326b427bbba9d3ec3a8e996dcf19ad111152a8fc
josh <joshua@marinacci.org>
2021-05-04 15:14:49 -0700

    use symbol font for the dock

--- src/clients/dock.js
+++ src/clients/dock.js
@@ -1,49 +1,53 @@
 import {App, Component, Label, VBox} from './guitoolkit.js'
 import {DEBUG} from 'idealos_schemas/js/debug.js'
+import {INPUT} from 'idealos_schemas/js/input.js'
 
 export class IconButton extends Component {
     constructor(opts) {
         super(opts)
         this.text = opts.text || "icon"
         this.appname = opts.appname
+        this.pressed = false
     }
 
     layout(gfx) {
-        let met = gfx.text_size(this.text)
+        let met = gfx.text_size(this.text,this.font)
         this.width = 16
         this.height = 16
     }
 
-    input(mouse, keyboard) {
-        if(!mouse.inside(this.x,this.y,this.width,this.height)) return false
-        if(mouse.down && !this.pressed) {
-            console.log('down action')
-            app.send(DEBUG.MAKE_StartAppByName({
-                name:this.appname
-            }))
+    input(evt) {
+        if(evt.type === INPUT.TYPE_MouseDown) {
+            app.send(DEBUG.MAKE_StartAppByName({ name:this.appname }))
+            this.pressed = true
+            this.repaint()
         }
-        this.pressed = mouse.down
+        if(evt.type === INPUT.TYPE_MouseUp) {
+            this.pressed = false
+            this.repaint()
+        }
         return true
     }
 
     redraw(gfx) {
-        gfx.rect(this.x, this.y, this.width, this.height, 'white')
-        gfx.text(this.x=2, this.y, this.text, 'black')
+        let bg = this.pressed?"red":"white"
+        let fg = this.pressed?"white":"black"
+        gfx.rect(this.x, this.y, this.width, this.height, bg)
+        gfx.text(this.x+2, this.y, this.text, 'black',this.font)
     }
 }
 
 let app = new App(process.argv)
 async function init() {
     await app.a_init()
-    app.log("done initting. opening window")
     let win = await app.open_window(0, 0, 16, 16 * 5, 'dock')
     win.root = new VBox({
         width: 16,
         height: 16 * 5,
         children: [
-            new IconButton({text: "F",appname:'fractal'}),
-            new IconButton({text: "G",appname:'guitest'}),
-            new IconButton({text: "C",appname:'dotclock'}),
+            new IconButton({text: "s",appname:'fractal', font:app._symbol_font}),
+            new IconButton({text: "t",appname:'guitest', font:app._symbol_font}),
+            new IconButton({text: "r",appname:'dotclock', font:app._symbol_font}),
         ]
     })
 }
