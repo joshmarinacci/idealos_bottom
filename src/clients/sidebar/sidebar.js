08cf4c3a7814a695c05ec37f0dba3a3ddb73ffef
josh <joshua@marinacci.org>
2021-05-27 16:46:13 -0700

    more sidebar work added screenshot

--- src/clients/sidebar/sidebar.js
+++ src/clients/sidebar/sidebar.js
@@ -7,7 +7,7 @@
  */
 
 
-import {App} from '../toolkit/guitoolkit.js'
+import {App, Component} from '../toolkit/guitoolkit.js'
 import {VBox} from '../toolkit/panels.js'
 import {Container} from '../toolkit/guitoolkit.js'
 
@@ -23,15 +23,25 @@ class TimeDatePanel extends Container {
         super.redraw(gfx)
         let ts = new Date()
         // time pm date
-        gfx.rect(this.x, this.y, this.width, this.height,'black')
-        gfx.rect(this.x+1, this.y+1, this.width-2, this.height-2,'white')
-        gfx.text(this.x+1, this.y+1, ts.toDateString(), 'black')
-        gfx.text(this.x+1, this.y+10, ts.toDateString(), 'black')
+        gfx.rect(this.x,this.y,this.width,this.height,'black')
+        gfx.rect(this.x+1,this.y+1,this.width-2,this.height-2,'white')
+        gfx.text(this.x+2, this.y+0, ts.toLocaleTimeString(), 'black')
+        gfx.text(this.x+2, this.y+9, ts.toLocaleDateString(), 'black')
     }
 }
 
-class MusicPlayerPanel extends Container {
+class MusicPlayerPanel extends Component {
+    constructor(opts) {
+        super(opts);
+    }
+    redraw(gfx) {
+        gfx.rect(this.x,this.y,this.width,this.height,'black')
+        gfx.rect(this.x+1,this.y+0,5,this.height-1,'white')
+        gfx.rect(this.x+2,this.y+1,3,10,'black')
 
+        gfx.text(this.x+10,this.y+1,'Hey Jude','white')
+        gfx.text(this.x+10,this.y+15,'Past Masters - The Beatles','white')
+    }
 }
 
 class CPUInfoPanel extends Container {
@@ -42,14 +52,16 @@ class CPUInfoPanel extends Container {
     }
     layout(gfx) {
         this.width = 80
-        this.height = 20
+        this.height = 15
     }
     redraw(gfx) {
         super.redraw(gfx)
+        gfx.rect(this.x,this.y,this.width,this.height,'black')
+        gfx.rect(this.x+1,this.y,this.width-2,this.height-1,'white')
         let bg = 'black'
         this.data.forEach((v,i)=>{
-            let vv = Math.floor(v*this.height)
-            gfx.rect(this.x+i*2, this.y+this.height-vv, 1, vv, bg)
+            let vv = Math.floor(v*(this.height-2))
+            gfx.rect(this.x+i*2, this.y+this.height-vv-2, 1, vv, bg)
         })
     }
 
@@ -60,8 +72,19 @@ class CPUInfoPanel extends Container {
     }
 }
 
-class WeatherPanel extends Container {
-
+class WeatherPanel extends Component {
+    constructor(opts) {
+        super(opts);
+    }
+    redraw(gfx) {
+        //fill white
+        gfx.rect(this.x,this.y,this.width,this.height,'black')
+        gfx.rect(this.x+1,this.y,this.width-2,this.height-1,'white')
+        //draw icon
+        gfx.rect(this.x+2,this.y+2,10,10,'black')
+        //show temp
+        gfx.text(this.x+20, this.y, '45o','black')
+    }
 }
 
 class NotificationsPanel extends Container {
@@ -70,18 +93,24 @@ class NotificationsPanel extends Container {
 
 async function init() {
     await app.a_init()
-    let win = await app.open_window(50,50,100,200,'plain')
-    win.root = new VBox({width:100, height:300, children:[
+    let win = await app.open_window(50,50,80,200,'plain')
+    win.root = new VBox({width:80, height:300, children:[
             new TimeDatePanel({
                 width:80,
-                height:20,
+                height:22,
             }),
-            // new MusicPlayerPanel(),
+            new MusicPlayerPanel({
+                width:80,
+                height:30,
+            }),
             new CPUInfoPanel({
                 width:80,
-                height:20,
+                height:15,
             }),
-            // new WeatherPanel(),
+            new WeatherPanel({
+                width:80,
+                height:15,
+            }),
             // new NotificationsPanel(),
         ]})
 }
